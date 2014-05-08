3f65daf665c31b85e8a68a17b7f24967a0ffba63
Tuomas Suutari <tuomas.suutari@andersinno.fi>
2014-05-08 19:36:57 +0300

    My customization

---
+++ personal/custom.el
@@ -0,0 +1,68 @@
+;;; custom.el --- Personal Emacs configuration stuff
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'prelude-packages)
+
+;; Packages ================================================
+
+;; elpy
+(prelude-require-packages '(elpy))
+(require 'elpy)
+(elpy-enable)
+(elpy-use-ipython)
+(elpy-clean-modeline)
+
+;; helm-git-grep
+(prelude-require-packages '(helm-git-grep))
+
+;; jabber
+(prelude-require-packages '(jabber))
+
+;; Global keys =============================================
+
+;; Revert to default C-a behavior
+(global-set-key
+ [remap move-beginning-of-line] 'move-beginning-of-line)
+
+;; ace-jump-mode
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
+;; helm
+;;(global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-c C-<SPC>") 'helm-all-mark-rings)
+;;(global-set-key (kbd "M-x") 'helm-M-x)
+
+;; helm-git-grep
+(global-set-key (kbd "C-c C-g") 'helm-git-grep-at-point)
+;; helm-git-grep - Invoke `helm-git-grep' from isearch.
+(define-key isearch-mode-map (kbd "C-c g") 'helm-git-grep-from-isearch)
+;; helm-git-grep - Invoke `helm-git-grep' from other helm.
+(eval-after-load 'helm
+  '(define-key helm-map (kbd "C-c g") 'helm-git-grep-from-helm))
+
+;; Emacs variables and faces  ==============================
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elpy-rpc-backend "jedi")
+ '(python-fill-docstring-style (quote django))
+ '(server-mode t)
+ '(server-temp-file-regexp "^/tmp/Re\\|/draft$\\|/COMMIT_EDITMSG$\\|/MERGE_MSG$\\|/git-rebase-todo$")
+ '(winner-mode t nil (winner))
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-diff-add ((((class color) (background dark)) (:foreground "green"))))
+ '(magit-item-highlight ((t (:inherit secondary-selection :background "#011" :foreground "white"))) t)
+ )
+
+(load "~/.emacs.d/personal/private.el")
