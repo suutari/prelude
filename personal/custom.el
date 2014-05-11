c4c1ce3aebdbc5e33bc75951503a73e279e759ce
Tuomas Suutari <tuomas@nepnep.net>
2014-05-12 02:17:20 +0300

    Add .git directory to dired-omit-files

--- personal/custom.el
+++ personal/custom.el
@@ -61,6 +61,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\.git$")
  '(elpy-rpc-backend "jedi")
  '(python-check-command "flake8")
  '(python-fill-docstring-style (quote django))
