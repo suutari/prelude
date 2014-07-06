e546435b2a8243520902d14a18a6744e985c688e
Tuomas Suutari <tuomas@nepnep.net>
2014-07-06 15:19:04 +0300

    personal/custom.el: Formatting changes by Emacs

--- personal/custom.el
+++ personal/custom.el
@@ -96,8 +96,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-diff-add ((((class color) (background dark)) (:foreground "green"))))
- '(magit-item-highlight ((t (:inherit secondary-selection :background "#011" :foreground "white"))) t)
- )
+ '(magit-diff-add ((((class color) (background dark)) (:foreground "green"))) t)
+ '(magit-item-highlight ((t (:inherit secondary-selection :background "#011" :foreground "white"))) t))
 
 (load "~/.emacs.d/personal/private.el")
