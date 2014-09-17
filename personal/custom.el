4b9636d9ba78c5cf9e349e1570e2145a1cc99c39
Tuomas Suutari <tuomas.suutari@andersinno.fi>
2014-09-17 09:49:08 +0300

    personal: Set fill column to 72

--- personal/custom.el
+++ personal/custom.el
@@ -13,6 +13,7 @@
  '(custom-safe-themes (quote ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\.git$")
  '(elpy-rpc-backend "jedi")
+ '(fill-column 72)
  '(persp-auto-resume-time 0.5)
  '(persp-set-last-persp-for-new-frames nil)
  '(prelude-clean-whitespace-on-save nil)
