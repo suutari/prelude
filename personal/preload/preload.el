e970849c5fd176b91ddcfe122fd7c060cdaff0eb
Tuomas Suutari <tuomas@nepnep.net>
2015-06-30 18:15:48 +0300

    Add marmalade ELPA source

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -1,3 +1,9 @@
+;; Add Marmalade to end of ELPA package archives list
+(require 'package)
+(add-to-list
+ 'package-archives
+ '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
 ;; Disable recentf-auto-cleanup before recentf is started
 (require 'recentf)
 (setq recentf-auto-cleanup 'never)
