716d99f77b9f8c057450d0d7943866c83db6e0c5
Tuomas Suutari <tuomas.suutari@anders.fi>
2017-09-08 09:26:12 +0300

    personal: Configure "suomi" ispell dictionary

--- personal/extra.el
+++ personal/extra.el
@@ -50,6 +50,15 @@
  'tramp-default-proxies-alist
  (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
 
+;; Spelling
+
+(setq ispell-local-dictionary-alist
+      (append ispell-local-dictionary-alist
+              '(("suomi"
+                 "[%.0-9A-Za-z\247\300-\326\330-\366\370-\377-]"
+                 "[^.%0-9A-Za-z\247\300-\326\330-\366\370-\377-]"
+                 "[':]" nil nil nil utf-8))))
+
 
 ;; Fonts and stuff =========================================
 
