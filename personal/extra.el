f3e71e464f65fdea85362e32132faf804dc0c5b5
Tuomas Suutari <tuomas.suutari@anders.fi>
2018-02-20 12:56:41 +0200

    personal: Configure Tide

--- personal/extra.el
+++ personal/extra.el
@@ -66,15 +66,42 @@
 
 
 ;; Tide (TypeScript IDE) ===================================
-
+(prelude-require-packages '(tide typescript-mode web-mode))
+(require 'tide)
+(require 'typescript-mode)
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; setup Tide for TS files
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+;(add-hook 'before-save-hook 'tide-format-before-save)
+
+;; setup Tide for TSX files
 (add-hook 'web-mode-hook
           (lambda ()
             (when (string-equal "tsx" (file-name-extension buffer-file-name))
               (setup-tide-mode))))
 ;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
+
+;; Error message formats
 
 (add-to-list 'compilation-error-regexp-alist
              '("^\\[\\([0-9]+\\), \\([0-9]+\\)\\]: [^\n]*" nil 1 2))
