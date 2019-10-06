97527f1982c5cd15ad5f5d490285207f1ab6e39e
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-10-06 14:13:36 +0300

    prelude-editor: Disable super-sae-mode

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -152,7 +152,7 @@
 (require 'super-save)
 ;; add integration with ace-window
 (add-to-list 'super-save-triggers 'ace-window)
-(super-save-mode +1)
+;(super-save-mode +1)
 
 (defadvice set-buffer-major-mode (after set-major-mode activate compile)
   "Set buffer major mode according to `auto-mode-alist'."
