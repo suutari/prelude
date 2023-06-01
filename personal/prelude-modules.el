ec39440d1fc25a576335d5752a207384696fe84f
Tuomas Suutari <tuomas.suutari@anders.fi>
2023-06-01 18:34:00 +0300

    personal/prelude-modules: Add prelude-lsp

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -18,6 +18,7 @@
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-latex)
 (require 'prelude-lisp)
+(require 'prelude-lsp) ;; Base setup for the Language Server Protocol
 ;; (require 'prelude-mediawiki)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 ;; (require 'prelude-perl)
