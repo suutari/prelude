eb6ef4a94456ba33d37abb0effb8d95f1cf901a6
Tuomas Suutari <tuomas.suutari@anders.fi>
2023-07-30 18:41:49 +0300

    Revert "personal: Set default font height to 110"
    This reverts commit 03796651c35fd8bce404c6ff889618cb44261046.

--- personal/extra.el
+++ personal/extra.el
@@ -87,7 +87,7 @@
 
 ;; Fonts and stuff =========================================
 
-(set-face-attribute 'default nil :height 110)
+(set-face-attribute 'default nil :height 90)
 
 
 ;; Tide (TypeScript IDE) ===================================
