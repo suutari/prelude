ef7af2c6af4459318c629102f3e1f09d05c12494
Tuomas Suutari <tuomas@nepnep.net>
2026-04-28 08:00:44 +0300

    personal/extra: Remove ispell stuff
    Seems that ispell is no longer installed.

--- personal/extra.el
+++ personal/extra.el
@@ -69,16 +69,6 @@
  'tramp-default-proxies-alist
  (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
 
-;; Spelling
-
-(setq ispell-local-dictionary-alist
-      (append ispell-local-dictionary-alist
-              '(("suomi"
-                 "[%.0-9A-Za-z\247\300-\326\330-\366\370-\377-]"
-                 "[^.%0-9A-Za-z\247\300-\326\330-\366\370-\377-]"
-                 "[':]" nil nil nil utf-8))))
-
-
 ;; Fonts and stuff =========================================
 (set-face-attribute 'default nil :height 90)
 
