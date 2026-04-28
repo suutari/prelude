f2e475e8a79abd8a1623229780e9254e00c7b7b6
Tuomas Suutari <tuomas@nepnep.net>
2026-04-28 07:59:48 +0300

    personal/eglot-config: Add more python server programs

--- personal/eglot-config.el
+++ personal/eglot-config.el
@@ -1,13 +1,35 @@
 (use-package eglot
-  :config
-  (add-to-list 'eglot-server-programs
-               '(python-mode . ("pylsp")))
+    :config
+    (add-to-list 'eglot-server-programs
+        `((python-mode python-ts-mode) .
+             ,(eglot-alternatives
+                  '(
+                       ("pylsp")
+                       ("basedpyright-langserver" "--stdio")
+                       ("zuban" "server")
+                       ))))
+    (setq-default eglot-workspace-configuration
+        '(
+             :pylsp
+             (
+                 :plugins
+                 (:ruff (:enabled t :formatEnabled t)))
+             :basedpyright (:typeCheckingMode "recommended")
+             :basedpyright.analysis
+             (
+                 :diagnosticSeverityOverrides
+		 (
+		     :reportUnusedCallResult "none")
+                 :inlayHints
+                 (
+                     ;; :callArgumentNames :json-false
+                     ;; :functionReturnTypes :json-false
+                     ;; :variableTypes :json-false
+                     ;; :genericTypes :json-false
+                     ))
+             ))
+)
 
-  ;; pylsp settings: enable plugins and configure them
-  (setq-default eglot-workspace-configuration
-                '(:pylsp (
-                          :plugins (
-                                    :ruff (
-                                           :enabled t
-                                           :formatEnabled t
-                                           ))))))
+;; For removing the previous python-mode server programs
+;; (setq eglot-server-programs
+;;      (assoc-delete-all '(python-mode python-ts-mode) eglot-server-programs))
